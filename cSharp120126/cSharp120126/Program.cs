d86447e2a89e793612b31665c47807b7e9242551
kja <krzysztofjanjasinski@gmail.com>
2026-01-12 13:32:07 +0100

    #1 - menu principal

--- cSharp120126/cSharp120126/Program.cs
+++ cSharp120126/cSharp120126/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using cSharp120126.Struct;
+
+Console.WriteLine("Bienvenu dane le programme ");
+string choix = "9";
+while (choix != "0")
+{
+    Console.WriteLine("Faitez votre choix entre :\n" +
+        "1. Gérer les Clients\n" +
+        "2. Gérer les Voyages\n" +
+        "3. Gérer les Réservations \n" +
+        "0. Quitter\n");
+    Console.Write("Mettez votre choix ==> ");
+    choix = Console.ReadLine();
+    switch (choix)
+    {
+        case "1":
+            Console.WriteLine("Gestion");
+            break;
+        case "2":
+            Console.WriteLine("Voyages");
+            break;
+        case "3":
+            Console.WriteLine("Resa");
+            break;
+        case "0":
+            Console.WriteLine("Aurevoir");
+            break;
+        default:
+            Console.WriteLine("Choix invalide");
+            break;
+    }
+}

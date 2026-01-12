0fbe5cb30fe5d9fde8aad9acad178e1f4416cd36
kja <krzysztofjanjasinski@gmail.com>
2026-01-12 14:12:14 +0100

    ajouts listes

--- cSharp120126/cSharp120126/Model/StructSet.cs
+++ cSharp120126/cSharp120126/Model/StructSet.cs
@@ -3,5 +3,8 @@ namespace  cSharp120126.Model
     public static class StructSet
     {
         public static List<cSharp120126.Struct.Client> Clients = [];
+        public static List<cSharp120126.Struct.Voyage> Voyages = [];
+        public static List<cSharp120126.Struct.Reservation> Reservations = [];
     }
+
 }

1548a0f12e394937879b3e9e1c4029b34eb70c98
gca <cher_gia@hotmail.com>
2026-01-12 14:20:30 +0100

    #3 client crud operations

---
+++ cSharp120126/cSharp120126/Model/StructSet.cs
@@ -0,0 +1,8 @@
+namespace  cSharp120126.Model
+{    
+    public static class StructSet
+    {
+        public static List<cSharp120126.Struct.Client> Clients = [];
+        public static int ClientIdCounter = 0;
+    }
+}
--- cSharp120126/cSharp120126/Struct/Client.cs
+++ cSharp120126/cSharp120126/Struct/Client.cs
@@ -2,13 +2,21 @@
 {
     public struct Client
     {
+        public int uId;
         public string Prenom;
         public string Nom;
 
-        Client(string prenom, string nom)
+        public Client(string prenom, string nom)
         {
+            uId = ++ cSharp120126.Model.StructSet.ClientIdCounter;
+            
             Prenom = prenom;
             Nom = nom;
         }
+
+        public override string ToString()
+        {
+            return $"[Client] uId: {uId}, Prenom: {Prenom}, Nom: {Nom}";
+        }        
     }
 }
